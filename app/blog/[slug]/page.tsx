f85e7af8b3f0ad0b4a36fa30336de16cb4f55e75
MdHasib01 <md.hasibuzzaman001@gmail.com>
2025-05-31 03:23:56 +0600

    fix force static

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -201,6 +201,7 @@ export function generateStaticParams() {
 }
 
 // export const output = "static";
+export const dynamic = "force-static";
 
 // Sample blog data - in a real app, this would come from a database or CMS
 const samplePosts = [
